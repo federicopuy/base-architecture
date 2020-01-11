329edc6745e0076eaa92a36bf61d5269dfdc152d
federicopuy <federicop07@hotmail.com>
2020-01-10 21:20:21 -0300

    Added okhttp logging interceptor (#6)

--- app/src/main/java/com/android/federicopuy/basearquitecture/MainNetwork.kt
+++ app/src/main/java/com/android/federicopuy/basearquitecture/MainNetwork.kt
@@ -1,15 +1,23 @@
 package com.android.federicopuy.basearquitecture
 
 import com.android.federicopuy.basearquitecture.model.Character
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+
 private val service: MainNetwork by lazy {
+
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.apply { interceptor.level = HttpLoggingInterceptor.Level.BODY }
+    val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
     val retrofit = Retrofit.Builder()
         .baseUrl("https://swapi.co/api/")
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
